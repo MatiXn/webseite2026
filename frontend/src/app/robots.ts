9f982d01ab590ebe5f7014e88ae005a5f9d185cc
Matien <matin.askaryar@phe-perm.de>
2026-07-11 08:41:59 +0200

    fix: use non-www domain in sitemap and robots for GSC compatibility

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [{ userAgent: "*", allow: "/", disallow: ["/api/", "/_next/"] }],
-    sitemap: "https://www.phe-perm.de/sitemap.xml",
-    host: "https://www.phe-perm.de",
+    sitemap: "https://phe-perm.de/sitemap.xml",
+    host: "https://phe-perm.de",
   };
 }
